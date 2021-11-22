209a1537863337cdf71b15d754fce7fd33b0f956
Aboelsayed78 <aboelsaied78@yahoo.com>
2021-11-22 03:25:32 +0200

    deploy (commit: 11605ef2553ba4dd9e613c90e6e14b2f47dc8958)

---
+++ precache-manifest.205db959329b74b6193bb94d5ad9fe0e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa76bd19e25b6f46e05",
+    "url": "/grad-project/css/app.f602146a.css"
+  },
+  {
+    "revision": "7d3d7b50025ed8d55a74",
+    "url": "/grad-project/css/chunk-vendors.d24f59aa.css"
+  },
+  {
+    "revision": "4dc994b580a44bf6af6425fc3501e4c0",
+    "url": "/grad-project/img/agency-1.4dc994b5.jpg"
+  },
+  {
+    "revision": "1238c0896db5d176cb9414599b896e0f",
+    "url": "/grad-project/img/header-1.1238c089.jpg"
+  },
+  {
+    "revision": "cde4a2bec0a6d01f27fc837168391695",
+    "url": "/grad-project/img/header-2.cde4a2be.jpg"
+  },
+  {
+    "revision": "c17d152425def85a9989d4c0c86f539d",
+    "url": "/grad-project/img/header-3.c17d1524.jpg"
+  },
+  {
+    "revision": "c0195b2579a4ad6c9f2c0bbfc7c5dc63",
+    "url": "/grad-project/img/header-4.c0195b25.jpg"
+  },
+  {
+    "revision": "e96601661c8990436f34db78f4877e97",
+    "url": "/grad-project/img/header-5.e9660166.jpg"
+  },
+  {
+    "revision": "76099e67c4bbc39ebde4518fe4346801",
+    "url": "/grad-project/img/header-7.76099e67.jpg"
+  },
+  {
+    "revision": "497cd7313fe9522fc9a01a30890453aa",
+    "url": "/grad-project/img/header-8.497cd731.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/grad-project/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "726c2c7e53fc5d6618389b042dd5ba51",
+    "url": "/grad-project/img/types.726c2c7e.jpg"
+  },
+  {
+    "revision": "90a362111bef129786325e33904c65dd",
+    "url": "/grad-project/index.html"
+  },
+  {
+    "revision": "5aa76bd19e25b6f46e05",
+    "url": "/grad-project/js/app.251bc393.js"
+  },
+  {
+    "revision": "7d3d7b50025ed8d55a74",
+    "url": "/grad-project/js/chunk-vendors.53f12b19.js"
+  },
+  {
+    "revision": "6f8d555669acbc46ecd89ad00bec27fe",
+    "url": "/grad-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/grad-project/robots.txt"
+  }
+]);
